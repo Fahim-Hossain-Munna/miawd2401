a5a4ebad9efa1404e9b5b5495c2e2b14503feee1
fahim hossain munna <suger.puffs12@gmail.com>
2024-08-24 11:08:42 +0600

    new changes

---
+++ backend/portfolio/store.php
@@ -0,0 +1,59 @@
+<?php
+
+include '../../config/database.php';
+
+session_start();
+
+if(isset($_POST['insert'])){
+    $id = $_SESSION['author_id'];
+    $title = $_POST['title'];
+    $subtitle = $_POST['subtitle'];
+    $description = $_POST['description'];
+    $image = $_FILES['image']['name'];
+    $explode = explode('.',$image);
+    $extension = end($explode);
+    $tmp_name = $_FILES['image']['tmp_name'];
+
+    if($title && $subtitle && $description && $image){
+        $newname = $id . '-' . $title . '-'. date('d-m-Y').'-'.rand(0,9999).'.'.$extension;
+
+        $localpath = '../../public/uploads/portfolio/'.$newname;
+
+        if(move_uploaded_file($tmp_name,$localpath)){
+            $insert_query = "INSERT INTO portfolios (title,subtitle,description,image) VALUES ('$title','$subtitle','$description','$newname')";
+
+            mysqli_query($db,$insert_query);
+
+            $_SESSION['port_success'] = "New Portfolio Insert Successfully Complete";
+
+            header('location: portfolios.php');
+        }
+    }
+
+}
+
+
+
+if(isset($_GET['deleteid'])){
+    $id = $_GET['deleteid'];
+    $port_query = "SELECT * FROM portfolios WHERE id='$id'";
+    $connect = mysqli_query($db,$port_query);
+    $port = mysqli_fetch_assoc($connect);
+
+    if($port['image']){
+        $oldname = $port['image'];
+        $existspath = '../../public/uploads/portfolio/'.$oldname;
+
+        if(file_exists($existspath)){
+            unlink($existspath);
+        }
+    }
+
+    $delete_query = "DELETE FROM portfolios WHERE id='$id'";
+    mysqli_query($db,$delete_query);
+    $_SESSION['port_success'] = "Portfolio Delete Successfully Complete";
+
+    header('location: portfolios.php');
+}
+
+?>
